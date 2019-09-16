a556c3c53d1a22ab3ff2b4aafaf7042b706e6564
Tyler <nemethtyler@gmail.com>
2019-09-15 22:40:51 -0400

    newsoup.py

--- newsoup.py
+++ newsoup.py
@@ -1,24 +1,45 @@
 # import HTMLSession from requests_html
-from requests_html import HTMLSession
+import requests
 from bs4 import BeautifulSoup
 import csv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import numpy
+
+
  
 for l in range(0,100000):
-# create an HTML Session object
-	session = HTMLSession()
-	print('Connecting to Webpage')
-	# Use the object above to connect to needed webpage
-	resp = session.get("http://connect2concepts.com/connect2/?type=circle&key=8E2C21D2-6F5D-45C1-AF9E-C23AEBFDA68B")
-	 
-	print('Render JavaScript')
-	# Run JavaScript code on webpage
-	resp.html.render()
 
-	soup = BeautifulSoup(resp.html.html, "lxml")
+	def create_browser(webdriver_path):
+	    #create a selenium object that mimics the browser
+		browser_options = Options()
+	    #headless tag created an invisible browser
+		browser_options.add_argument("--headless")
+		browser_options.add_argument('--no-sandbox')
+		browser = webdriver.Chrome(webdriver_path, chrome_options=browser_options)
+		print("Done Creating Browser")
+		return browser
+
+	url = "http://connect2concepts.com/connect2/?type=circle&key=8E2C21D2-6F5D-45C1-AF9E-C23AEBFDA68B"
+	browser = create_browser('/users/nemet/chromedriver.exe') #DON'T FORGET TO CHANGE THIS AS YOUR DIRECTORY
+	browser.get(url)
+	innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
+	page_html = browser.page_source
+	print(innerHTML)
+
+# create an HTML Session object
+#	print('Connecting to Webpage')
+	# Use the object above to connect to needed webpage
+#	resp = requests.get("http://connect2concepts.com/connect2/?type=circle&key=8E2C21D2-6F5D-45C1-AF9E-C23AEBFDA68B")
+	 
+#	print('Render JavaScript')
+	# Run JavaScript code on webpage
+#	resp.html.render()
+
+	soup = BeautifulSoup(page_html, "lxml")
 	 
 	info = soup.find_all('div', attrs={'style': 'text-align:center;'})
 
